8f5c0c128de655c334f73128ceebfa7652dce2eb
mac <20j.code@gmail.com>
2024-01-11 18:45:21 +0900

    save

---
+++ log.md
@@ -0,0 +1,18 @@
+# cpp stl 종류
+
+pair : first와 second로 구성된 컨테이너
+
+vector : 배열과 비슷, 맨 끝 삽입,삭제 위치 참조 O(1)
+중간 삽입 삭제 O(n)
+
+deque
+
+list
+
+forward_list
+
+set
+
+map
+
+https://ssocoit.tistory.com/24
--- main.cpp
+++ main.cpp
@@ -1,6 +1,41 @@
 #include <iostream>
+#include <map>
+
+using namespace std;
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    int n = 0; // 반에 있는 학생 수
+
+    int min_ans = 0;
+    int max_ans = 0;
+
+    cin>>n;
+
+    map<int,string> db;
+
+    for(int i=0;i<n;i++){
+        //초기화
+        string human;
+        int dd = 0;
+        int mm = 0;
+        int yy = 0;
+        int year = 0;
+        cin>>human>>dd>>mm>>yy;
+
+        //입력시 나이를 1990-01-01부터 계산해서 저장한다.
+        year = dd+(mm-1)*30+(yy-1990)*365;
+        //year가 높을수록 나이가 낮게 된다.
+
+        db.insert({year,human});
+
+    }
+
+    for(auto iter=db.begin();iter!=db.end();iter++){
+
+        if(iter->first < min_ans){
+
+        }
+
+    }
 }
